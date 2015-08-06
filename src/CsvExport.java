f6b41fa12e93400d9f0a8cc85ec99b40e665ba85
Sebastian Haas <sebastian.haas@buergermail.net>
2015-08-06 15:43:40 +0200

    Merge branch 'master' of https://github.com/p-grau/pricecalculator
    Conflicts:
    	src/Marketplace.java
    	src/Rounding.java

---
+++ src/CsvExport.java
@@ -0,0 +1,50 @@
+/**
+ * Created by pgrau on 06/08/15.
+ */
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class CsvExport
+{
+	public static FileWriter writer;
+	public static File file;
+
+	public static void writeCSV(String ItemNo, double salesPriceGross) throws IOException
+	{
+		// File anlegen
+		file = new File("/Users/pgrau/Desktop/Preiskalkulation.csv");
+		try
+		{
+			// new FileWriter(file ,true) - prüft ob die Datei vorhanden ist
+			// wenn ja wird am Ende weiter geschrieben
+			// new FileWriter(file) - wird überschrieben
+			writer = new FileWriter(file, true);
+
+			// Text wird in den Stream geschrieben
+			writer.write("Hallo Wie gehts?");
+
+			// Platformunabhängiger Zeilenumbruch wird in den Stream geschrieben
+			writer.write(System.getProperty("line.separator"));
+
+			// Text wird in den Stream geschrieben
+			writer.write("Danke mir gehts gut!");
+
+
+			// Schreibt den Stream in die Datei
+			// Sollte immer am Ende ausgeführt werden, sodass der Stream
+			// leer ist und alles in der Datei steht.
+			writer.flush();
+
+			// Schließt den Stream
+			writer.close();
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+}
+
+
